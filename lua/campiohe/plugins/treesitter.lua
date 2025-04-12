3e3f7371bf2faee594b5733ff1bfb1a26ee0f0d8
campiohe <campiottihenrique@gmail.com>
2025-04-12 19:40:39 -0300

    adding treesitter config

--- lua/campiohe/plugins/treesitter.lua
+++ lua/campiohe/plugins/treesitter.lua
@@ -1,1 +1,15 @@
-return { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "c", "lua", "python", "markdown", "markdown_inline" },
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
+}
